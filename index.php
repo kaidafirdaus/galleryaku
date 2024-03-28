5d94cf4103aaea9718e98e2500e25d937b48f755
unknown <riskanuranggraeni172@gmail.com>
2024-03-28 11:44:53 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website galeri Foto</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Dafrtar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
